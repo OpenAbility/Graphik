4958fecb2e7be63bbdfbb8956b9928e85b2b9d8f
khhs167 <khhs1671@gmail.com>
2023-04-05 19:38:54 +0200

    Generated IGraphikAPI

--- OpenAbility.Graphik/IGraphikAPI.cs
+++ OpenAbility.Graphik/IGraphikAPI.cs
@@ -1,5 +1,7 @@
+// Auto-generated from GLAPI from OpenAbility.Graphik.OpenGL
+// These should not be modified
 namespace OpenAbility.Graphik;
-
+using OpenAbility.Graphik;
 public interface IGraphikAPI
 {
 	void InitializeWindow(string title, int width, int height);
