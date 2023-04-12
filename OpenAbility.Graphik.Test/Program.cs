e41bc2362da39f9daca272675af4ab892cbef4cf
khhs167 <khhs1671@gmail.com>
2023-04-12 17:02:29 +0200

    Test now uses the platform-agnostic Graphik API wrapper

--- OpenAbility.Graphik.Test/Program.cs
+++ OpenAbility.Graphik.Test/Program.cs
@@ -1,18 +1,16 @@
 ﻿using OpenAbility.Graphik;
 using OpenAbility.Graphik.OpenGL;
 using StbImageSharp;
-using System;
-using System.IO;
 
-GLAPI graphik = new GLAPI();
+Graphik.SetAPI(new GLAPI());
+Graphik.InitializeSystems();
 
-graphik.SetErrorCallback((id, message) =>
+Graphik.SetErrorCallback((id, message) =>
 {
 	Console.WriteLine(id + ": " + message);
 });
 
-graphik.InitializeSystems();
-graphik.InitializeWindow("Hello, World Graphik!", 1280, 720);
+Graphik.InitializeWindow("Hello, World Graphik!", 1280, 720);
 
 float[] vertices =
 {
@@ -23,14 +21,14 @@ float[] vertices =
 	 0.5f,  0.5f, 1.0f, 1, 1,
 };
 
-IMesh mesh = graphik.CreateMesh();
+IMesh mesh = Graphik.CreateMesh();
 mesh.PrepareModifications();
 mesh.SetVertexData(vertices);
 mesh.SetVertexAttrib(0, 3, VertexAttribType.Float, 5 * sizeof(float), 0);
 mesh.SetVertexAttrib(1, 2, VertexAttribType.Float, 5 * sizeof(float), 3 * sizeof(float));
 mesh.SetIndices(new uint[] {0, 1, 2, 1, 3, 2});
 
-IShader shader = graphik.CreateShader();
+IShader shader = Graphik.CreateShader();
 shader.AttachSource(File.ReadAllText("assets/test.vert"), "test.vert", ShaderType.VertexShader);
 Thread.Sleep(100);
 shader.AttachSource(File.ReadAllText("assets/test.frag"), "test.frag", ShaderType.FragmentShader);
@@ -51,33 +49,33 @@ ImageResult imageResult = ImageResult.FromMemory(File.ReadAllBytes("assets/logo.
 
 byte[] imageData = imageResult.Data;
 
-ITexture texture = graphik.CreateTexture();
+ITexture texture = Graphik.CreateTexture();
 texture.PrepareModifications();
 texture.SetData(TextureFormat.Rgba8, imageData, imageResult.Width, imageResult.Height);
 
-GLRenderTexture renderTexture = graphik.CreateRenderTexture();
+IRenderTexture renderTexture = Graphik.CreateRenderTexture();
 renderTexture.Build(512, 512);
 
-while (!graphik.WindowShouldClose())
+while (!Graphik.WindowShouldClose())
 {
-	graphik.InitializeFrame();
-	graphik.Clear(ClearFlags.Colour | ClearFlags.Depth);
+	Graphik.InitializeFrame();
+	Graphik.Clear(ClearFlags.Colour | ClearFlags.Depth);
 	
 	renderTexture.Target();
-	graphik.Clear(ClearFlags.Colour | ClearFlags.Depth);
+	Graphik.Clear(ClearFlags.Colour | ClearFlags.Depth);
 	shader.Use();
 	texture.Bind();
 	shader.BindInt("tex", 0);
 	mesh.Render(6);
 	
-	graphik.ResetTarget();
-	graphik.Clear(ClearFlags.Colour | ClearFlags.Depth);
+	Graphik.ResetTarget();
+	Graphik.Clear(ClearFlags.Colour | ClearFlags.Depth);
 	shader.Use();
 	renderTexture.Bind(RenderTextureComponent.Colour);
 	shader.BindInt("tex", 0);
 	mesh.Render(6);
 	
-	graphik.FinishFrame();
+	Graphik.FinishFrame();
 }
 
 return 0;
